61edb4d5cfffa29dfd1ab996e48b85f5f73e2801
abinrushud <abinrushud@tahakom.com>
2023-02-07 11:58:34 +0300

    complete the project

---
+++ README.md
@@ -0,0 +1,23 @@
+<!-- Add banner here -->
+
+# Udacity Landing Page
+
+This project requires you to create a multi-section landing page with a dynamically updating navigational menu based on the amount of content added to the page.
+# Demo-Preview
+
+- 
+
+- [Udacity Landing Page](#udacity-landing-page)
+- [Demo-Preview](#demo-preview)
+- [Resources](#resources)
+
+# Resources
+
+I used some ideas from the following pages to develop my project in addition to all I learned from the course materials.
+
+- https://stackoverflow.com/questions/2735067/how-to-convert-a-dom-node-list-to-an-array-in-javascript
+- https://stackoverflow.com/questions/56409340/keyframes-gradient-background-is-not-animated
+- https://linuxhint.com/scroll-to-id-javascript/
+- https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+- https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+- https://awik.io/check-if-element-is-inside-viewport-with-javascript/
--- js/app.js
+++ js/app.js
@@ -1,12 +1,58 @@
+// Define Global Variables 
 const sections = document.getElementsByTagName('section');
 const navBar = document.getElementById('nav-bar-list');
+const listItems = document.getElementsByTagName('li');
 
+// Main function to call all needed functions to start the process
 function main() {
+  setupNavBar();
+  document.addEventListener('DOMContentLoaded', isInViewport)
+  document.addEventListener('scroll', isInViewport)
+}
+
+// setupNavBar function is to create the nav dynamically 
+function setupNavBar() {
   const sectionArray = Array.from(sections);
+
   sectionArray.forEach((section, index) => {
+    const name = `Section ${index + 1}`;
     const navItem = document.createElement('li');
-    navItem.textContent = `Section ${index + 1}`
+
+    navItem.textContent = name
     navBar.appendChild(navItem);
+    if (index === 0) {
+      navItem.classList.add('active');
+    }
+
+    navItem.addEventListener('click', () => {
+      section.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+}
+
+// clearActive function is to clear the active nav state
+function clearActive() {
+  const listArray = Array.from(listItems);
+
+  listArray.forEach((listItem, Index) => {
+    listItem.classList.remove('active');
+  });
+}
+
+// isInViewport is to dynamically change the nav state active class 
+function isInViewport() {
+  const sectionArray = Array.from(sections);
+  const listArray = Array.from(listItems);
+
+  sectionArray.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    const top = rect.top - 100;
+    const bottom = rect.top + rect.height - 100;
+
+    if (top <= window.innerHeight && bottom <= window.innerHeight) {
+      clearActive();
+      listArray[index].classList.add('active');
+    }
   });
 }
 
